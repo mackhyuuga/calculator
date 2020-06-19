ca2ea55d4b8832ea8272db75f8183c33d8d0fa69
allison eduardo <mackhyuuga@hotmail.com>
2020-06-19 16:54:38 -0300

    add map, reduce and list comprehensions.

--- basic_operations.py
+++ basic_operations.py
@@ -1,15 +1,18 @@
-import numpy
+from functools import reduce
 
-def add(numbers):
-    return sum(numbers)
+def introduction():
+    print('{:=^50}'.format('  calculator  '))
+    print('\n\n select the desired operation number\n\n')
+    print("1 - add \n2 - subtraction \n3 - multiplication \n4 - devision \n")
+    option = int(input('type your option: '))
+    print('\n')
+    return option
 
-def subtraction(numbers):
-    for i in range(len(numbers) - 1):
-        numbers[i + 1] = -1*numbers[i + 1]
-    return sum(numbers)
+def add(x, y):
+    return x + y
 
-def multiplication(numbers):
-    return numpy.prod(numbers)
+def multiplication(x, y):
+    return x * y
 
 def division(numbers):
     fractional = numbers[0] / numbers[1]
@@ -18,37 +21,34 @@ def division(numbers):
 
     return (fractional, integer, rest)
 
-print('{:=^50}'.format('  calculator  '))
-print('\n\n select the desired operation number\n\n')
-print("1 - add \n2 - subtraction \n3 - \
-multiplication \n4 - devision \n")
-
-option = int(input('type your option: '))
-print('\n')
+option = introduction()
 
 numbers = []
 
 if option == 1:
     numbers.append(float(input("type the first numbers: ")))
     while True:
-        n = input('type another number, if you dont want to put more, type "no": ')
+        n = input('type another number, if you dont want to put more type "no": ')
         if n == 'no':
              break
         else:
             numbers.append(float(n))
 
-    print(f'the sum of these terms is : {add(numbers)}')
+    print(f'the sum of these terms is : {reduce(add, numbers)}')
 
 elif option == 2:
     numbers.append(float(input("type the first numbers: ")))
     while True:
-        n = input('type another number, if you dont want to put more, type "no": ')
+        n = input('type another number, if you dont want to put more type "no": ')
         if n == 'no':
              break
         else:
             numbers.append(float(n))
 
-    print(f'the subtraction of terms by the first is : {subtraction(numbers)}')
+        result = [-x for x in numbers[1:]]
+        result.append(numbers[0])
+
+    print(f'the subtraction of terms by the first is : {reduce(add, result)}')
 
 elif option == 3:
     numbers.append(float(input("type the first numbers: ")))
@@ -59,7 +59,7 @@ elif option == 3:
         else:
             numbers.append(float(n))
 
-    print(f'the multiplication between these terms is : {multiplication(numbers)}')
+    print(f'the multiplication between these terms is : {reduce(multiplication, numbers)}')
 
 elif option == 4:
     numbers.append(float(input("type the first numbers: ")))
